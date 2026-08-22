be2eb20257c77609e81b50d77714f89e89808415
nbnguyen75 <nbnguyen.dev@gmail.com>
2026-08-22 12:36:15 +0700

    feat: update ci/cd script

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -3,6 +3,5 @@ import app from '@/app';
 export default {
 	idleTimeout: Number(process.env.IDLE_TIMEOUT) || 15,
 	port: Number(process.env.PORT) || 5001,
-	hostname: '0.0.0.0',
 	fetch: app.fetch
 };
