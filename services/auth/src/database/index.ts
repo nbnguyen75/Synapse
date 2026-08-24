a44a5a254636cd9f5ec2a74eb54d9fc3d2e0fde0
nbnguyen75 <nbnguyen.dev@gmail.com>
2026-08-25 00:51:23 +0700

    fix: try to fix ssl connection for db in auth

--- services/auth/src/database/index.ts
+++ services/auth/src/database/index.ts
@@ -4,8 +4,14 @@ import { Pool } from 'pg';
 import { relations } from '@/database/relations';
 import { env } from '@/env';
 
+const needsSsl = env.DATABASE_URL.includes('sslmode=');
+
 const pool = new Pool({
-	connectionString: env.DATABASE_URL
+	ssl: needsSsl ? { rejectUnauthorized: false } : false,
+	connectionString: env.DATABASE_URL,
+	connectionTimeoutMillis: 2000,
+	idleTimeoutMillis: 30000,
+	max: 10
 });
 
 export const db = drizzle({ client: pool, jit: true, relations });
